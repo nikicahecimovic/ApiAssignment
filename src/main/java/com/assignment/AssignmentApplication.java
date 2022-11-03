4ec511b336ec2d87685160dd28ae3b7615e3335b
nikicahecimovic <nikica.hecimovic@gmail.com>
2022-11-03 12:14:23 +0100

    Remove unused prints

--- src/main/java/com/assignment/AssignmentApplication.java
+++ src/main/java/com/assignment/AssignmentApplication.java
@@ -9,12 +9,7 @@ import java.time.LocalDateTime;
 public class AssignmentApplication {
 
 	public static void main(String[] args) {
-		LocalDateTime a = LocalDateTime.now();
 		SpringApplication.run(AssignmentApplication.class, args);
-		System.out.println("Hello world");
-		LocalDateTime b = LocalDateTime.now();
-		System.out.println(a + " " + b);
-		System.out.println(a.isAfter(b));
 	}
 
 }
